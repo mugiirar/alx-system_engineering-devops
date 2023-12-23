b7ff5fd0766f33d0a1d1f2b227f3f17dad8252c8
mugiirar <munenevincentmugiira@gmail.com>
2023-12-23 04:22:47 -0800

    school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-file { '/tmp/school':
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet'
+#create a file in tmp
+
+file { 'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
