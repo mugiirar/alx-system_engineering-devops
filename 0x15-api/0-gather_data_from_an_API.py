c809e64f9f2cdeb43cd7dd358848c44d7cbcc195
mugiirar <munenevincentmugiira@gmail.com>
2024-01-29 04:54:07 -0800

    completed

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""listing information"""
+import sys
+import requests
+
+
+url = "https://jsonplaceholder.typicode.com/"
+user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+work = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+completed = []
+
+for k in work:
+    if k.get("completed") is True:
+        completed.append(k.get("title"))
+
+
+print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(work)))
+
+for com in completed:
+    print("\t {}".format(com))
