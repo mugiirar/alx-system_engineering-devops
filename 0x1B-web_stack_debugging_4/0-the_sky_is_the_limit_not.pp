7434fa3d69cec2d55be210292f1f49b39dc149a0
mugiirar <munenevincentmugiira@gmail.com>
2024-02-21 01:41:18 -0800

    im so done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# traffic increase
+
+exec { 'fix--nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
