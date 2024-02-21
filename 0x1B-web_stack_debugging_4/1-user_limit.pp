7ed144a6e206f2df7f06f20003c0ccea39c05f46
mugiirar <munenevincentmugiira@gmail.com>
2024-02-21 01:55:15 -0800

    user;

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# open files without error.
+
+# hard file limit for Holberton user.
+exec { 'hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# file limit for Holberton user.
+exec { 'limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
